3da31a33b6e2091e68853780b9bda281ebccd671
kovac <kovacsbela958@gmail.com>
2020-05-26 12:13:49 +0200

    Created the database connection

--- src/ecoSolutionsShop/Data/DBConnection.java
+++ src/ecoSolutionsShop/Data/DBConnection.java
@@ -1,4 +1,39 @@
 package ecoSolutionsShop.Data;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+
 public class DBConnection {
+    private static Connection connect;
+    private static final DBConnection instance = new DBConnection();
+
+
+
+
+    private DBConnection() {
+
+        try {
+
+            Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+
+            this.connect = DriverManager.getConnection("jdbc:sqlserver://localhost:1433; databaseName=Ecosolutions", "sa", "123456");
+
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     *
+     * @return
+     */
+
+    public static Connection getConnect() {
+        return connect;
+    }
+
+    public static DBConnection getInstance() {
+        return instance;
+    }
 }
