cce447ba5e728c4eb829fce74d35818740a10de1
KendeSzabo <szabo.kende20@gmail.com>
2020-05-27 17:22:06 +0200

    added a verification process to the login + added a ShopID.java class because this was the only way it worked

--- src/ecoSolutionsShop/Controller/LoginController.java
+++ src/ecoSolutionsShop/Controller/LoginController.java
@@ -1,20 +1,34 @@
 package ecoSolutionsShop.Controller;
 
+import ecoSolutionsShop.Account.ShopAccount;
+import ecoSolutionsShop.Data.DBMethods;
 import ecoSolutionsShop.Main;
+import ecoSolutionsShop.Model.ShopID;
 import ecoSolutionsShop.View.UIControl.Controller;
 import ecoSolutionsShop.View.UIControl.windows;
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class LoginController implements Initializable, windows {
 
 
+    ShopAccount shopAccount = new ShopAccount();
+    //DBMethods dbMethods = new DBMethods();
     Controller myController;
-    private String shopID;
-    private String password;
+
+    @FXML
+    TextField shopIDTextfield,passwordTextfield;
+    @FXML
+    Label errorLabel;
+
+    private String shopID = "";
+    private String password = "";
+
 
 
     @Override
@@ -24,11 +38,26 @@ public class LoginController implements Initializable, windows {
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
-
     }
 
-    // set the New Order window
+
+    // set the New Order window if login credentials are correct
     public void login(ActionEvent actionEvent) {
-        myController.setWindow(Main.windowId2);
+        shopID = shopIDTextfield.getText();
+        shopAccount.setShopID(shopID);
+        //dbMethods.setShopID(shopIDTextfield.getText());
+        //System.out.println(dbMethods.shopID);
+        ShopID.getInstance().shopID = shopID;
+        password = passwordTextfield.getText();
+        shopAccount.setPassword(password);
+        if (shopAccount.verifyCredentials()==true){
+            myController.setWindow(Main.windowId2);
+        }
+        else{
+            errorLabel.setText("Incorrect shop ID or Password!");
+        }
+
+
+
     }
 }
--- src/ecoSolutionsShop/Data/DBMethods.java
+++ src/ecoSolutionsShop/Data/DBMethods.java
@@ -1,13 +1,34 @@
 package ecoSolutionsShop.Data;
 
+import ecoSolutionsShop.Account.ShopAccount;
+import ecoSolutionsShop.Controller.LoginController;
+import ecoSolutionsShop.Model.ShopID;
+
+import java.lang.reflect.Field;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
 public class DBMethods {
+
+
+    ShopID shopIDObject = new ShopID();
+    public String shopID = "";
+    private String password = "";
+
+    public void setShopID(String shopID) {
+        this.shopID = shopID;
+    }
+    public String getShopID() {
+        return shopID;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+    /*
     String name = "";
     int sellPrice = 0;
-
     public void testMethod(){
         try {
             PreparedStatement query = DBConnection.getConnect().prepareStatement("SELECT * FROM tblClothingType WHERE fldClothingTypeID = ?");
@@ -24,6 +45,7 @@ public class DBMethods {
         }
         System.out.println(name + sellPrice);
     }
+    */
 
     public void selectShop() {
 
@@ -48,4 +70,21 @@ public class DBMethods {
     public void insertClient(){
 
     }
+    //selects password from tblShop for a shopIS which is entered into the login form
+    public void selectCredentials(){
+        System.out.println(shopID);
+        try {
+            PreparedStatement query = DBConnection.getConnect().prepareStatement("SELECT * FROM tblShop WHERE fldDeliveryPonintID = ?"); //!!!!be careful there is a typo in the field name
+            query.setString(1, shopIDObject.getInstance().shopID);
+            ResultSet resultSet = query.executeQuery();
+            if (resultSet.next()) {
+                shopID = resultSet.getString(1);
+                password = resultSet.getString(4);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+
 }
