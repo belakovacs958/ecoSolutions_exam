85be92e0d2137580e588d7354f218b86afaa2ef6
KendeSzabo <szabo.kende20@gmail.com>
2020-06-02 11:24:38 +0200

    (This is actually working now, laundry item getters are needed)table view is working + creates an invoice file + organized + added comments

--- src/ecoSolutionsShop/Model/LaundryItem.java
+++ src/ecoSolutionsShop/Model/LaundryItem.java
@@ -2,13 +2,34 @@ package ecoSolutionsShop.Model;
 
 public class LaundryItem {
 
-    private String description, itemStatus, clothingTypeName;
-    private int laundryItemID;
+    private String description = "";
+    private String itemStatus = "";
+    private String clothingTypeName = "";
+    private int laundryItemID = 0;
 
     public LaundryItem(String description, int laundryItemID, String itemStatus, String clothingTypeName) {
         this.description = description;
         this.laundryItemID = laundryItemID;
         this.itemStatus = itemStatus;
         this.clothingTypeName = clothingTypeName;
+    }
+
+    //!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!THE GETTERS ARE USED AND THE TABLE VIEW IS NOT WORKING WITHOUT THEM!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
+
+    public String getDescription() {
+        return description;
+    }
+
+    public String getItemStatus() {
+        return itemStatus;
+    }
+
+    public String getClothingTypeName() {
+        return clothingTypeName;
+    }
+
+    public int getLaundryItemID() {
+        return laundryItemID;
     }
 }
