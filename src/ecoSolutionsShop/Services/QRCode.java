1710bfb48d51221b4485c26690570cfbe4a870c7
KendeSzabo <szabo.kende20@gmail.com>
2020-06-08 16:18:33 +0200

    corrected qr code text

--- src/ecoSolutionsShop/Services/QRCode.java
+++ src/ecoSolutionsShop/Services/QRCode.java
@@ -10,7 +10,7 @@ public class QRCode {
     public void generateQRCode(int laundryItemID){
         try{
             file = new Formatter("C:/Users/KendeSzabo/IdeaProjects/ecoSolutions_exam/src/ecoSolutionsShop/Services/QRCodes/" + laundryItemID + ".txt");
-            file.format(String.valueOf(laundryItemID));
+            file.format(String.valueOf("the laundry item ID is the number which is coded into the QR code. The ID is:   " + laundryItemID));
             file.close();
 
         }
